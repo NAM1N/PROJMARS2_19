966aa1d1148291960a2a7f99083287f7d870140b
NAM1N <48988955+NAM1N@users.noreply.github.com>
2019-03-27 19:48:47 +0100

    index.php

---
+++ index.php
@@ -0,0 +1,230 @@
+
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+ <!-- mes fichiers-->
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" name="viewport" content="width=device-width,initial-scale=1"  />
+<link rel="stylesheet" href="CSS/style.css" type="text/css" />
+<link rel="shortcut icon" href="images/NSI.ico" /> 
+<meta name="description" content="Niw&eacute;tian Services Informatiques, société spécialisée dans le développement de solutions logicielles spécifiques, construit VOTRE logiciel sur mesure selon VOS besoins." />
+	  <meta name="keywords" content="logiciel sur mesure, developpement, informatique, vienne, conception logiciel informatique" />
+		<meta name="author" content="Niw&eacute;tian Services Informatiques" />
+<!-- fiohier exporté-->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+<link rel="stylesheet"href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+
+
+<script>
+
+$(document).ready(function() {
+	
+	 $('.deroul1').hide();
+	 // afficher le menu 
+	$('.recet').hover(function(){
+		$('.deroul1').show();
+		
+		});
+	// cache le	
+	$('.hom').hover(function(){
+		$('.deroul1').hide();
+		
+		});	
+	$('.sant').hover(function(){
+		$('.deroul1').hide();
+		
+		});	
+		
+	$('.bud').hover(function(){
+		$('.deroul1').hide();
+		
+		});	
+   $('.recet1').hover(function(){
+		$('.deroul1').hide();
+		
+		});	
+ $('.conect').hover(function(){
+		$('.deroul1').hide();
+		
+		});	
+ $('.abon').hover(function(){
+		$('.deroul1').hide();
+		
+		});	
+		$('.command').hover(function(){
+		$('.deroul1').hide();
+		
+		});
+		///
+		 $('.deroul2').hide();
+	 // afficher le menu 
+	
+	$('.command').hover(function(){
+		$('.deroul2').show();
+		
+		});
+	// cache le	
+	$('.recet').hover(function(){
+		$('.deroul2').hide();
+		
+		});
+	$('.sant').hover(function(){
+		$('.deroul2').hide();
+		
+		});	
+	$('.bud').hover(function(){
+		$('.deroul2').hide();
+		
+		});	
+   $('.recet1').hover(function(){
+		$('.deroul2').hide();
+		
+		});	
+ $('.conect').hover(function(){
+		$('.deroul2').hide();
+		
+		});	
+ $('.abon').hover(function(){
+		$('.deroul2').hide();
+		
+		});	
+  $('.hom').hover(function(){
+		$('.deroul2').hide();
+		
+		});	
+		
+    
+});
+</script>
+
+<!-- script jquery-->
+<!-- fiohier CSS-->
+<link href='https://fonts.googleapis.com/css?family=Open+Sans+Condensed:300,700' rel='stylesheet' type='text/css'>
+<style>
+ /* ecran normal */
+/* les plices */
+#bloc1
+{
+	font-family: 'Open Sans Condensed', sans-serif;
+	font-size:18px;color:#515457;
+}
+/*les liens debut*/
+ a
+{
+	color:#FFF;
+	font-weight:bold;
+	text-decoration:none;
+	font-family:'Open Sans Condensed', sans-serif;
+	font-size:30px;
+	border-radius:5px 
+
+}
+.enf:hover{
+	border-left:4px solid #0F6;
+	padding-left:5px;
+	color:#F90;
+	text-decoration:none;
+	 }
+ .adult:hover{
+	border-left:4px solid #999;
+	padding-left:5px;
+	
+	color:#F90;
+	text-decoration:none;
+	 }
+.lien .recet:hover,.recet1:hover
+{
+	border-bottom:#F60 solid 4px;
+	
+
+}
+.lien .command:hover{
+	border-bottom:4px solid #999;
+	
+	}
+
+
+p{ color:#fff}
+.para{
+	font-size:24px;
+
+	}
+.para1{
+	font-size:14px
+	}
+/*les liens fin*/
+/*les polices */
+.titre{
+	color:#F30}
+label{
+	font-size:18px;
+	color:#FFF
+}
+ 
+/*formuliares */
+input.transparent-input{
+       background-color:rgba(0,0,0,0) !important;
+       border: 2px solid #FFF;
+	   color:#FFF;
+	   font-size:18px;
+	   font-weight:bold
+	    !important;}
+ /* ecran mobil */
+ @media all and (max-width:1024px) 
+ {
+	#form1{
+		
+		}
+ }
+   
+</style>
+
+<!-- les police -->
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Cantarell">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Rancho&effect=shadow-multiple">
+<!--nom du site  -->
+<title>NF | Nam1N FOOD</title>
+</head>
+  
+ </header>
+      
+      
+      <section >
+       <div class="container-fluid" style=" background:url(images/ord3.png) scroll no-repeat center ; background-size:cover; " >
+         <div class="row" style="background-color:rgba(0,0,0,0.4);margin:5px 70px 5px 70px; padding-bottom:-100%">
+         
+           <div class="col-md-2" style="border-right:4px solid #FFF; margin:30px">
+           
+           
+           <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</p><p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</p><p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</p><p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</p><p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</p><p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</p><p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</p><p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</p><p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</p><p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</p><p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</p><p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</p><p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</p>
+           <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</p><p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</p><p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</p><p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</p><p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</p><p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</p>
+            </div>
+            <div class="col-md-8" style="margin-top:20px">
+              <img src="images/log.png"  class="img-responsive" style="width:100%; margin:20px" />
+              <h1 style="color:#FFF">Bienvenue sur le Site RESTO NF | Nam1N FOOD</h1>
+              <p class="para1" >Nous sommes une jeune structure dynamique sp&eacute;cialis&eacute; dans la vente en ligne de met et les ingredient de cuisine africaine   travers la C&ocirc;te d'Ivoire <br />
+                                   Nous offrons un acceuil chalereux a nos client...<br />
+                                   Depuis 2ans Nous Transportons des repas de qualit&eacute; et Recevons des produits de qualit&eacute; en toute Securit&eacute; 
+                                 
+                                   
+                                   </p>
+           <p class="para"><span class="glyphicon glyphicon-ok"></span> Faites votre march&eacute; quotidien ou mensuel tout en restant chez Vous</p> 
+           <p class="para"><span class="glyphicon glyphicon-ok"></span> Commandez vos repas chaud  tout en restant chez Vous</p> 
+           <p class="para"><span class="glyphicon glyphicon-ok"></span> Echanger Vos secret de cuisine avec notre communaut&eacute; dynamique et devenez un axe en cuisine...</p> 
+                                    <p>&nbsp;&nbsp;</p>
+                                    <div style="margin:2%">
+                               <a href="connexion.php" class="con1 btn btn-success btn-lg" >CONNEXION</a>
+                               &nbsp;&nbsp;&nbsp;&nbsp;
+              <a href="abonne.php" class="con2 btn btn-primary btn-lg">S'ABONNER</a> </div>   
+                                   
+              
+            </div>
+         </div>  
+        </div> 
+      </section>
+<footer ><!-- pied de page  -->
+   
+  </footer>
+</body>
+</html>
